e849126dd7be253cc4e75cee63e65e3b4d37cc6d
Thabo Moeketsi <thabo.moeketsi@pbtgroup.co.za>
2024-02-23 17:33:31 +0200

    registration

--- website/views.py
+++ website/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'home.html',{})
+    # Check to see if loggin in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        #authenticate
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request,"you have successfully logged in ")
+            return redirect('home')
+        else:
+            messages.success(request,"There was an error loggin in please try again .... ")
+            return redirect('home')
+    else:
+        return render(request, 'home.html',{})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,"logged out")
+    return redirect('home')
+
+def register_user(request):
+  return render(request, 'register.html',{})
